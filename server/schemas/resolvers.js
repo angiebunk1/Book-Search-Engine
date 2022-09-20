3c7350628c310fab1ccd7725ea53407ecd8c93c2
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-20 15:03:42 -0500

    added resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,65 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        user: async (parent, {username}) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('savedBooks')
+        },
+
 
     },
 
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+
+        },
+        saveBook: async (parent, { bookId }, context) => {
+            if (context.user) {         
+
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks : bookId } },
+                { new: true }
+              ).poplulate('savedBooks');
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to be logged in!');
+          },
+        deleteBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                {_id: user._id },
+                { $pull: { savedBooks: bookId }},
+                { new: true }
+                ).populate('savedBooks');
+
+                return updatedUser;
+              }
+              throw new AuthenticationError('You need to be logged in!'); 
+        }
 
     }
 };
